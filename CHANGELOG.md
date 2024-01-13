cf26898fff7bd5bfd9b7e2df7d8692a81c946b86
Danlois Tovar <danlois.tovar@gmail.com>
2024-01-12 21:29:02 -0300

    build: :wrench: :heavy_plus_sign: Se configura ambiente de desarrollo
    Add config del ambiente de dev:Eslint, Babel, Prettier, lint-staged, husky, Git hooks y changelogs.
    
    Nota: de aqui en adelante se debe crear rama dev, para empezar el desarollo.

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.0 (2024-01-12)
+
+-   build: :tada: Inicio de Proyecto. ([3d3fea3](https://github.com/DanloisTovar/app-heroes/commit/3d3fea3))
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+	return (
+		<>
+			<h1>Aplicacion Heroes</h1>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est
+				amet dolores dolorem magnam libero dolor fugiat. Veniam, ipsa
+				cumque temporibus ducimus praesentium id sapiente culpa
+				suscipit, doloremque magnam quas fugit.
+			</p>
+		</>
+	);
+}
+
+export default App;
