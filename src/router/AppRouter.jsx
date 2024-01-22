11145d9ffc26c9271d393920391deb897cffeea1
Danlois Tovar <danlois.tovar@gmail.com>
2024-01-22 20:49:29 -0300

    feat: :hammer: Navbar
    Se inserto Navbar en el router.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Marvelpage, DCpage } from '../heroes/pages/';
 import { LoginPage } from '../auth/pages/';
+import { Navbar } from '../UI/components';
 export const AppRouter = () => {
 	return (
 		<>
+			<Navbar />
 			<Routes>
 				<Route path="/marvel" element={<Marvelpage />} />
 				<Route path="/dc" element={<DCpage />} />
