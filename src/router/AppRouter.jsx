25d5041092c8731ebf0061d834b28788d5435cb4
Danlois Tovar <danlois.tovar@gmail.com>
2024-01-22 20:34:32 -0300

    fix: :wrench: navigate
    Se agrega navigate a react rputer dom y se unsa para colocar la pagina de marvel, como comodin.
    
    Nota: se debe impotar Navigate en react router dom

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Marvelpage, DCpage } from '../heroes/pages/';
 import { LoginPage } from '../auth/pages/';
 export const AppRouter = () => {
@@ -8,6 +8,7 @@ export const AppRouter = () => {
 				<Route path="/marvel" element={<Marvelpage />} />
 				<Route path="/dc" element={<DCpage />} />
 				<Route path="/login" element={<LoginPage />} />
+				<Route path="/" element={<Navigate to="/marvel" />} />
 			</Routes>
 		</>
 	);
